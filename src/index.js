f24df63292f60bd00ab7a51c3ab4108adba7d743
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-26 23:37:16 -0500

    prettier and add keywords

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
 export default function multipleRender(component, selector) {
   if (document.querySelectorAll(selector).length >= 1) {
     const containers = [...document.querySelectorAll(selector)];
-		let components = [];
-
-    containers.forEach(el => {
-			let props = {};
-
-			try {
-				props = el.getAttribute('data-props')
-        ? JSON.parse(el.getAttribute('data-props'))
-        : {};
-			} catch(err) {
-				console.error('Check json structure on data-props', err);
-			}
-			
-			let componentElement = React.createElement(component, {...props});
-
-			if(React.isValidElement(componentElement)) {
-				render(componentElement, el);
-				components = components.concat([componentElement]);
-			} else {
-				console.error("check it's a valid component");
-			}
-      
-    });
+    let components = [];
 		
-		return components;
+    containers.forEach(el => {
+      let props = {};
+
+      try {
+        props = el.getAttribute("data-props")
+          ? JSON.parse(el.getAttribute("data-props"))
+          : {};
+      } catch (err) {
+        console.error("Check json structure on data-props", err);
+      }
+
+      let componentElement = React.createElement(component, { ...props });
+
+      if (React.isValidElement(componentElement)) {
+        render(componentElement, el);
+        components = components.concat([componentElement]);
+      } else {
+        console.error("check it's a valid component");
+      }
+
+    });
+
+    return components;
   }
 }
